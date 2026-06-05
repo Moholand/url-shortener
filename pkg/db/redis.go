f7a412bcf6ba17daceee354d7a12d4d2abe0aa9d
Mohammad Akbarzadeh <moho.akbarzadeh@gmail.com>
2026-06-05 11:08:55 +0330

    feat: add env for redis

--- pkg/db/redis.go
+++ pkg/db/redis.go
@@ -1,11 +1,17 @@
 package db
 
 import (
+	"os"
 	"github.com/redis/go-redis/v9"
 )
 
 func NewRedisClient() *redis.Client {
+	addr := os.Getenv("REDIS_ADDR")
+	if addr == "" {
+		addr = "redis:6379"
+	}
+
 	return redis.NewClient(&redis.Options{
-		Addr: "redis:6379", // container name in docker-compose
+		Addr: addr,
 	})
 }
